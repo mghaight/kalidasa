965bcc8768d7335847a26c68c4e08a46449dd9ba
Mackenzie Haight <mackenzie.haight@gmail.com>
2025-10-06 17:27:08 -0400

    cleaning logic for dcs_raw

--- data-raw/dcs_clean.R
+++ data-raw/dcs_clean.R
@@ -10,21 +10,46 @@ text_ids_to_cull <- c(
   60, # Commentary on Amaraughaśāsana
   113, # Amṛtabindūpaniṣat
   162, # Carakatattvapradīpikā
-  225, # Kāvyālaṃkāravṛtti
-  226, # Commentary on the Kāvyālaṃkāravṛtti
+  # 225, # Kāvyālaṃkāravṛtti
+  # 226, # Commentary on the Kāvyālaṃkāravṛtti
+  251, # Bhadrabāhucarita
   258, # Cakra (?) on Suśr
   275, # Aṣṭāṅgasaṃgraha
   317, # Khādiragṛhyasūtrarudraskandavyākhyā
-  379, # Abhidharmakośabhāṣya
+  379 # Abhidharmakośabhāṣya
 )
 
 dcs_meta <- dplyr::filter(dcs_meta, !(text_id %in% text_ids_to_cull))
 dcs <- dplyr::filter(dcs, !(text_id %in% text_ids_to_cull))
 
-# TODO
-# 1. `status` col in dcs_meta should be renamed `complete` and rows should either be `1`-complete or `0`-incomplete
-# 2.
+# preparing the raw dataset
+dcs_raw <- unique(dcs$text_id) |>
+  purrr::set_names() |>
+  purrr::map(function(t_id) {
+    text_chapters <- dplyr::filter(dcs, text_id == t_id) |>
+      purrr::pmap(function(...) {
+        ch <- list(...)
+        verse_raw <- rvest::read_html(ch$resp_body) |>
+          rvest::html_elements(".sentence_div") |>
+          rvest::html_text() |>
+          stringr::str_remove_all("Par\\.\\?") |>
+          stringr::str_squish()
 
-dcs_raw
+        verse_num <- stringr::str_extract(verse_raw, "\\([^)]*\\)") |>
+          stringr::str_remove_all("[()]")
 
-dcs_rich
+        tibble::tibble(
+          text = verse_raw |>
+            stringr::str_remove_all("\\([^)]*\\)$") |>
+            stringr::str_remove_all("/") |>
+            stringr::str_squish(),
+          maj_div = ch$maj_div,
+          min_div = ch$min_div,
+          sub_div = ch$sub_div,
+          verse_num = verse_num
+        )
+      }) |>
+      purrr::list_rbind()
+  })
+
+saveRDS(dcs_raw, file = "dcs_raw.rds")
